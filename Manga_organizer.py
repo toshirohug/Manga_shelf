f29a15322dba54f5de3c2b82d8c4d5f4ce65c6e4
toshirohug <hugotoshirosato@gmail.com>
2023-01-27 12:39:16 -0300

    Adding of: jsonServer, fixtures, parametrize and readme Updates

--- Manga_organizer.py
+++ Manga_organizer.py
@@ -1,45 +1,54 @@
 import requests
 import pytest
+import random
+from random import randint
+import json
 
-url = "https://my-json-server.typicode.com/toshirohug/Manga_shelf"
-payload = {
-            "id": 1,
-            "name": "Blue Lock",
-            "volume": "02",
-        }
+STATIC_DB = "https://my-json-server.typicode.com/toshirohug/Manga_shelf/shonen"
+SHONEN_ENDPOINT = "http://localhost:3000/shonen"
+SHOJO_ENDPOINT = "http://localhost:3000/shojo"
+KODOMOMUKE_ENDPOINT = "http://localhost:3000/kodomoMuke"
 
+@pytest.fixture
+def test_random_endpoint():
+    endpoints_list = SHONEN_ENDPOINT, SHOJO_ENDPOINT, KODOMOMUKE_ENDPOINT
+    random_endpoint = random.choice(endpoints_list)
+    return random_endpoint
 
-def test_01_storing_manga():
-    store_manga_response = requests.post(url, json=payload)
-    assert store_manga_response.status_code == 201
-    print(store_manga_response.json())
+@pytest.fixture
+def random_manga(random_endpoint):
+    selected_resource = requests.get(random_endpoint)
+    json_data = json.loads(selected_resource.text)
+    random_manga = random.choice(json_data)
+    return random_manga
 
-def test_02_replacing_manga():
-    replace_manga_response = requests.put(url, json=payload)
-    assert replace_manga_response.status_code == 201
-    print(replace_manga_response.json())
+manga_to_post = [{'id':55, 'name':'Yuyu Hakusho', 'volume':1}, {'id':17, 'name':'Saint Seiya', 'volume':2}, \
+{'id':18, 'name':'Saint Seiya', 'volume':3}, {'id':19, 'name':'Saint Seiya', 'volume':4}, {'id':20, 'name':'Saint Seiya', 'volume':5}]
 
-def test_03_check_for_singular_manga():
-    """
-    O que quero é: checar a LISTA_DE_DICIONARIOS por um VALOR e caso exista, IMPRIMIR o respectivo DICIONÁRIO
-    """
-    payload_wanted_manga =payload.get('name', 'Blue Lock')
-    print(payload_wanted_manga)
-    # check_manga_response = requests.get(url, json=payload_wanted_manga) 
-    # print(check_manga_response.json())
-    # assert check_manga_response.status_code == 200
+@pytest.mark.parametrize('manga', manga_to_post)
+def test_01_storing_manga(random_endpoint, manga):
 
-@pytest.mark.parametrize('endpoint, id, name, volume'['/shonen'])
-def test_04_check_all_shonen_mangas():
-    response = requests.get(url)
-    assert response.status_code == 200
-    print(response.text)
+    store_manga_response = requests.post(random_endpoint, manga)
+    print(store_manga_response)
+    assert store_manga_response.status_code == 201, "Check if the manga alread exists."
 
-# def test_02_replacing_manga():
-#     replace = requests.put(url, data={"Id": "1", "title": "Blue Lock", "volume": "02", "userId": "10"})
-#     print(replace.json())
-#     assert replace.status_code == 200
+def test_atualiza_resource(random_endpoint, random_manga):
 
-def get_all_mangas():
-    response = requests.get(https://my-json-server.typicode.com/toshirohug/Manga_shelf/Shonen)
-    shonen_list = response.text
+    manga_id = randint(0, 5)
+    random_endpoint = "{}/{}".format(random_endpoint, manga_id)
+    print(random_endpoint)
+    print(random_manga)
+    put_manga_response = requests.put(random_endpoint, data = random_manga)
+    print(put_manga_response.text)
+
+shonen_manga_data = [(1, 'Blue Lock', 1), (2, 'Blue Lock', 2), (3, 'Blue Lock', 3), (4, 'Blue Lock', 4), (5, 'Blue Lock', 5)]
+
+@pytest.mark.parametrize('manga_id, expected_manga_name, expected_manga_volume', shonen_manga_data)
+def test_04_check_all_shonen_mangas(manga_id, expected_manga_name, expected_manga_volume):
+    response = requests.get(f'https://my-json-server.typicode.com/toshirohug/Manga_shelf/shonen/{manga_id}')
+    body = response.text
+
+    str_expected_manga_volume = str(expected_manga_volume)
+    
+    assert expected_manga_name in body
+    assert str_expected_manga_volume in body
--- readme.md
+++ readme.md
@@ -1,6 +1,6 @@
 # Overview
 
-A python testing project that does validate the faker API (jsonplaceholder + my-json-server) CRUD workflows.
+A manga shelf testing project. Does validate POST | PUT | DELETE calls between either faker API (my-json-server) and JSON server (having instanced another resource to persist request operations).
 
 # Prerequisites
 
@@ -16,12 +16,35 @@ Creating of your API documentation into myjsonserver (see the link listed below 
 
 # SetUp
 
-Python 3.5 and higher
+Visual studio code - [download](https://code.visualstudio.com/download)
+
+Python 3.5 and higher - [download](https://www.python.org/downloads/)
 
 Pytest [Install](https://docs.pytest.org)
 
 Requests [Install](https://requests.readthedocs.io)
 
+## JSON server
+
+Under the folder you launched on Visual studio, enter the following commands (in that order) into terminal (I recommend strongly bash terminal):
+
+```sh
+$ mkdir json-server-lib
+
+$ cd json-server-lib
+
+$ npm init -y
+
+$ npm i -g json-server
+```
+
+## Starting JSON server
+
+The JSON server is started with the json-server:
+
+```sh
+$ json-server --watch mangas.json
+```
 # Usage
 
 Manga shelf is a API testing project composed of a collection of mangas ordened by categories: for young -> most common themes (Shonen) | for girls (Shojo) | for kids (KodomoMuke). Whenever the shelf owner is visiting their pretty and charming manga corner, he could take a look for appreciation, or grab one and left it at headboard and after, put another manga in the same position which the previous one was. He can also includding new volumes for any collections. Or else, he maybe withraw one of those mangas and for some reason, cannot put back (giving for someone, by loss...). For this purpose, we have the tests wrote to validate the get, put, post and delete calls for the fake API.
@@ -31,3 +54,5 @@ Manga shelf is a API testing project composed of a collection of mangas ordened 
 [jsonplaceholder API](https://jsonplaceholder.typicode.com/guide/)
 
 [Short guide to use the jsonplaceholder API with myjsonserver (PT)](https://medium.com/code-prestige/como-criar-um-a-api-rest-fake-para-testes-jsonplaceholder-7cc106ea3bd6)
+
+[Json server - a solution to emulating a simple API locally](https://zetcode.com/javascript/jsonserver/)
